bb2e0c0ac93b58d333c96aeacbc47114e71362cd
Ben Bolker <bbolker@gmail.com>
2019-09-29 23:23:26 -0400

    BMB comments

--- HW3.R
+++ HW3.R
@@ -36,7 +36,7 @@ fp1 <- ggplot(no_pain,
   geom_point() + geom_line() +
   scale_y_continuous(limits=c(0,1)) +
   labs(x="Months", y="Probability", title="Response: No Pain") +
-  scale_x_discrete(labels=c("1","2","3","4","5","6")) +
+  scale_x_discrete(labels=c("1","2","3","4","5","6"))
 
 p11 <-fp1 + guides(color = guide_legend(title.position = "top", title.hjust = 0.5)) +
   scale_color_discrete(
@@ -64,6 +64,9 @@ plot1 <-ggarrange(p11,fp2,fp3, nrow=1,ncol=3, common.legend = TRUE, legend = "bo
 annotate_figure(plot1, 
                 top = text_grob("Visualizing Conditional Item Response Probabilties", face="bold",size=14))
 
+## BMB: would definitely have been better to do this by faceting ...
+## did you consider using a directional colour ramp to indicate the
+## ordering of the latent classes? (see below)
 ##
   #Explain what features of the data you are trying to draw attention to,   
   #and what story you think your figures tell (or fail to tell).
@@ -80,8 +83,75 @@ annotate_figure(plot1,
 #probability of having no pain across the 6 months compared to those in the severe/chronic pain latent class who
 #have a lower probability of responding to no pain in the 6 months. Similar interpretations can be made 
 #for the remaining two graphs. Additionally, an important quality of latent class models
-#is high class separation. In these graphs, the classes are distingushable from each other (no lines crossing each other)
+#is high class separation. In these graphs, the classes are distinguishable from each other (no lines crossing each other)
 #thus it has high-class separation. Therefore I feel more confident of the labels/interpretation that the authors have 
 #given to the analysis. 
 
 
+## BMB: could have indicated size of clusters via line width
+library(tidyverse)
+dfm <- (df
+    %>% slice(-1)
+    %>% separate(Items,into=c("month","cat"),sep=1)
+    %>% pivot_longer(cols=C1:C4,names_to="class",values_to="pct")
+    %>% mutate(
+            cat=forcats::fct_inorder(cat),
+            month=as.numeric(month)
+        )
+)
+
+cc <- (df
+    %>% slice(1)
+    %>% pivot_longer(cols=C1:C4,names_to="class",values_to="n")
+    %>% select(-Items)
+)
+
+dfm <- (dfm
+    %>% full_join(cc,by="class")
+    %>% mutate(class=factor(class,levels=paste0("C",1:4),
+                            labels=c("Recovering",
+                                     "Persistent mild",
+                                     "Fluctuating",
+                                     "Severe chronic")))
+)
+
+library(colorspace)
+library(cowplot)
+theme_set(theme_cowplot()+
+          theme(panel.background = element_rect(fill = "#BFD5E3",
+                                                colour="#EEEEEE"),
+                panel.margin=grid::unit(0,"lines")))
+
+ggplot(dfm,
+       aes(x=month,y=pct, color=class)) +
+    geom_point() + geom_line(aes(size=n)) +
+    scale_y_continuous(limits=c(0,1)) +
+    labs(x="Months", y="Probability") +
+    facet_wrap(~cat) +
+    scale_colour_discrete_sequential(palette = "Reds")+
+    scale_x_continuous(breaks=c(1,3,6))+
+    scale_size_continuous(range=c(1,4),
+                          breaks=c(15,35))
+
+## BMB: less repetition, less data manipulation
+## changed background to make light colour visible
+## I wonder if there's a way to reinforce the compositional nature
+## of the data? (Consider Wilkie's sections on proportions)
+
+dfm <- dfm %>% group_by(cat,month) %>%
+    mutate(pct_norm=pct/sum(pct))
+
+## BMB: I don't quite understand why the numbers don't add up to 1
+##  in each column??
+
+ggplot(dfm,
+       aes(x=month,y=pct_norm, fill=class)) +
+    geom_area(position="stack") +
+    facet_wrap(~cat)+
+    scale_y_continuous(expand=c(0,0)) +
+    labs(x="Months", y="Probability") +
+    scale_fill_discrete_sequential(palette = "Reds")+
+    scale_x_continuous(breaks=c(1,3,6))
+## interesting but I would probably flip the order
+
+## generally a useful reformulation: score=2
