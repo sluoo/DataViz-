f67d6f9b809ba2549327cad7d13af14e26a4a479
Sherry Luo <sherryluo08@gmail.com>
2019-10-13 00:14:23 -0400

    HW4

---
+++ HW4.R
@@ -0,0 +1,69 @@
+library(tidyverse)
+#devtools::install_github("johannesbjork/LaCroixColoR")
+library(LaCroixColoR)
+library(readr)
+
+
+df <-read_csv("simulationsLCA.csv")
+
+#tidy dataset
+df1 <- (df 
+  %>% pivot_longer(cols=3:22,names_to="Class-IC",values_to="pct")
+  %>% separate(col="Class-IC",into = c("Class","IC"),sep="-")
+  %>% mutate(
+        SampleSize=as.factor(SampleSize),
+        IC=forcats::fct_inorder(IC),
+        Class=forcats::fct_inorder(Class)))
+ 
+#Re-labelling for clarity
+#State the true population parameter for each model
+df1 <- (df1 
+        %>% mutate(
+          PopulationLC = factor(PopulationLC,levels=sort(unique(PopulationLC)),
+                                labels = c("True Model K=3", 
+                                           "True Model K = 4"))))
+df1 <-(df1
+       %>% mutate(
+         Simulation = factor(Simulation,levels = paste0("Sim",c(4,1:3)),
+                              labels = c("10-Item (CUQ)",
+                                         "8-Item (SQ)",
+                                         "8-Item (SUQ)",
+                                        "15-Item (SE)"))))
+
+#Plot, label bars where percentage is greater than 10
+#Also change width of bar so it's not so squished
+g1 <- ggplot(df1, aes(x=SampleSize,y=pct, fill=Class)) + 
+  geom_bar(stat="identity",width=0.8,color="black",lwd=0.1) + 
+  facet_grid(PopulationLC + Simulation ~ IC) +
+  geom_text(aes(label=ifelse(pct >=10, paste0(pct,"%"),"")),size=3,
+            position=position_stack(vjust=0.5), colour="black")
+
+#Nice white theme to make the colors pop
+g1 <- g1 + 
+  scale_fill_manual(values=lacroix_palette("PassionFruit",type="discrete")) + 
+  labs(x="Sample Size", y= "Percentage") +
+  ggtitle("Percentage of Times the Lowest Value Occured for 500 replications in each model") + 
+  theme_classic() + theme(legend.position="bottom")
+
+g1 
+
+
+#Explaintion of design choice: 
+
+#Since we are comparing proportions, I believe a stacked bar plot best represented the results of 
+#the simulation.  The main goal of the study was to establish if a superior fit index for class 
+#enumeration existed. I felt placing the ICs at the top x-axis would highlight this goal better.
+#The true number of latent classes labelled with each model for more clarity. For a cleaner appearance, 
+#I chose the classic theme and outlined the bars in black. I added labels since some of the values
+#were difficult to differentiate such as graph row 1 column 1. I avoided plotting labels for values
+#less than 10. 
+
+
+
+
+
+
+
+
+
+
